8fadf23459142576e6d01f026582292c41cef8d4
Rozmusel <Bany1506@yandex.ru>
2024-08-07 15:00:34 +0300

    Menu

--- MCut/MCut.c
+++ MCut/MCut.c
@@ -1,21 +1,25 @@
 ﻿#include <stdio.h>
 #include <windows.h>
-
+#include <locale.h>
 #define NUMSIZE 16	// Максимальная длина считываемого числа
 #define VECSIZE 64	// Максимальное количество векторов
 #define STRSIZE 128	// Максимальная длина принимаемой строки
 
+enum colors { WHITE = 7, DARK_YELLOW = 6, GREEN = 10, DARK_RED = 4, GRAY = 8 } b1, b2, b3;
+int test;
+
+void menu();
 
 int main() {
-
 	FILE* Vectors;
 	FILE* Points;
 	FILE* Output;
 
 	errno_t read_res;	// Хранение результата открытия файла
+	b1 = b2 = b3 = WHITE;
 
-	enum colors { WHITE = 7, DARK_YELLOW = 6, GREEN = 10, DARK_RED = 4, GRAY = 8 };
 
+	setlocale(LC_ALL, "rus");
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(hConsole, DARK_YELLOW);	// Меняю цвет символов
 	printf(
@@ -40,5 +44,35 @@ int main() {
 		" @@@@@@@@@@@@@   @@@@@@@@@@@@@@@         @@@@		\n"
 		"                 @@@@@@@@@@@@@@@					\n"
 	);
+	printf("\n\n");
 	SetConsoleTextAttribute(hConsole, WHITE);
+
+	menu();
+}
+
+void menu() {
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	printf("[1] => ");
+	SetConsoleTextAttribute(hConsole, b1);
+	printf("Прочитать карту\n");
+	SetConsoleTextAttribute(hConsole, WHITE);
+
+	printf("[2] => ");
+	SetConsoleTextAttribute(hConsole, b2);
+	printf("Прочитать контур\n");
+	SetConsoleTextAttribute(hConsole, WHITE);
+
+	printf("[3] => ");
+	SetConsoleTextAttribute(hConsole, b3);
+	printf("Обрезать карту\n");
+	SetConsoleTextAttribute(hConsole, WHITE);
+
+	printf("<< ");
+	printf(	"\n-------------------------------------------------\n"
+			"\n-------------------------------------------------\n");
+	printf("\033[26;4H");
+
+	scanf_s("%d", &test);
+	return 0;
 }
