bd02c1bf9c7197e95df5c292b3904664966c66d3
Rozmusel <Bany1506@yandex.ru>
2024-08-07 10:09:48 +0300

    Добавьте файлы проекта.

---
+++ MCut/MCut.c
@@ -0,0 +1,43 @@
+﻿#include <stdio.h>
+#include <windows.h>
+
+#define NUMSIZE 16
+#define LINSIZE 64
+#define STRSIZE 128
+
+
+int main() {
+
+	FILE* Vectors;
+	FILE* Points;
+	FILE* Output;
+
+	errno_t read_res;
+
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN);
+	printf(
+ "                 @@@@@     @@@@@\n"
+ " @@@@@@@@@@@@@   @@@@@@@ @@@@@@@@    @@@@@@@@@@@@\n"
+ "@@@@@@@@@@@@@@  @@@@@@@@@@@@@@@@@    @@@@@@@@@@@@\n"
+ "@@@@@          @@@@@  @@@@@  @@@@@       @@@@\n"
+ "@@@@@         @@@@@           @@@@@      @@@@\n"
+ "@@@@@         @@@@             @@@@@     @@@@\n"
+ "@@@@@        @@@@@              @@@@     @@@@\n"
+ "@@@@@       @@@@@               @@@@@    @@@@\n"
+ "@@@@@      @@@@@ @@@@       @@@@ @@@@@   @@@@\n"
+ "@@@@@     @@@@@  @@@@       @@@@  @@@@@  @@@@\n"
+ "@@@@@     @@@@   @@@@       @@@@   @@@@@ @@@@\n"
+ "@@@@@       @@   @@@@       @@@@    @@   @@@@\n"
+ "@@@@@            @@@@       @@@@         @@@@\n"
+ "@@@@@            @@@@       @@@@         @@@@\n"
+ "@@@@@            @@@@       @@@@         @@@@\n"
+ "@@@@@            @@@@       @@@@         @@@@\n"
+ "@@@@@            @@@@       @@@@         @@@@\n"
+ "@@@@@@@@@@@@@@   @@@@       @@@@         @@@@\n"
+ " @@@@@@@@@@@@@   @@@@@@@@@@@@@@@         @@@@\n"
+ "                 @@@@@@@@@@@@@@@\n"
+);
+	SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN);
+	
+}
