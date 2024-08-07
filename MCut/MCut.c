a288d5e4d779710ebf240cff36a7f55ccd5ff6f3
Rozmusel <Bany1506@yandex.ru>
2024-08-07 17:12:18 +0300

    switch in menu

--- MCut/MCut.c
+++ MCut/MCut.c
@@ -6,9 +6,11 @@
 #define STRSIZE 128	// Максимальная длина принимаемой строки
 
 enum colors { WHITE = 7, DARK_YELLOW = 6, GREEN = 10, DARK_RED = 4, GRAY = 8 } b1, b2, b3;
-int test;
 
 void menu();
+int map();
+int contour();
+int cut();
 
 int main() {
 	FILE* Vectors;
@@ -51,28 +53,74 @@ int main() {
 }
 
 void menu() {
+	enum mode { POINTS = 1, VECTORS, CUTS };
+	int choice;
+
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
-	printf("[1] => ");
-	SetConsoleTextAttribute(hConsole, b1);
-	printf("Прочитать карту\n");
-	SetConsoleTextAttribute(hConsole, WHITE);
-
-	printf("[2] => ");
-	SetConsoleTextAttribute(hConsole, b2);
-	printf("Прочитать контур\n");
-	SetConsoleTextAttribute(hConsole, WHITE);
-
-	printf("[3] => ");
-	SetConsoleTextAttribute(hConsole, b3);
-	printf("Обрезать карту\n");
-	SetConsoleTextAttribute(hConsole, WHITE);
+	printf(	"[1] => Прочитать карту\n"
+			"[2] => Прочитать контур\n"
+			"[3] => Обрезать карту\n"	);
 
 	printf("<< ");
 	printf(	"\n-------------------------------------------------\n"
-			"\n-------------------------------------------------\n");
-	printf("\033[26;4H");
+			"\n-------------------------------------------------\n"	);
+	for (;;) {
+		SetConsoleTextAttribute(hConsole, WHITE);
+		printf("\033[26;4H");
+		printf("\33[K");
 
-	scanf_s("%d", &test);
+		scanf_s("%d", &choice);
+
+		printf("\033[28;0H");
+		printf("\33[K");
+		switch (choice) {
+		case POINTS:
+			if (map()) {
+				SetConsoleTextAttribute(hConsole, GREEN);
+				printf("Файл был успешно прочитан");
+				printf("\033[23;8H");
+				printf("Прочитать карту");
+			} else {
+				SetConsoleTextAttribute(hConsole, DARK_RED);
+				printf("Файл не был найден");
+				printf("\033[23;8H");
+				printf("Прочитать карту");
+			}
+			break;
+		case VECTORS:
+			if (contour()) {
+				SetConsoleTextAttribute(hConsole, GREEN);
+				printf("Файл был успешно прочитан");
+				printf("\033[24;8H");
+				printf("Прочитать контур");
+			}
+			else {
+				SetConsoleTextAttribute(hConsole, DARK_RED);
+				printf("Файл не был найден");
+				printf("\033[24;8H");
+				printf("Прочитать контур");
+			}
+			break;
+		case CUTS:
+			printf("Смена окна");
+			break;
+		default:
+			SetConsoleTextAttribute(hConsole, DARK_RED);
+			printf("                НЕИЗВЕСТНЫЙ РЕЖИМ");
+
+		}
+
+	}
+}
+
+int map() {
+	return 1;
+}
+
+int contour() {
+	return 1;
+}
+int cut() {
 	return 0;
 }
